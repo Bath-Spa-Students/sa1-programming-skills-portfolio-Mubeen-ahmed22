6a8303fab66b078f3bc268d38ba40bf6d5c5913d
Mubeen-ahmed22 <mrmaheen123@gmail.com>
2024-11-22 14:45:41 +0400

    list names.py

---
+++ z_Bonus_Exercises/list names.py
@@ -0,0 +1,7 @@
+#Store the names of a few of your friends in a list called names. Print each person’s nameby accessing each element in the list, one at a time.
+
+friends = ["Ahmed","Bilal","Kareem"]
+
+print(friends[0])
+print(friends[1])
+print(friends[2])
