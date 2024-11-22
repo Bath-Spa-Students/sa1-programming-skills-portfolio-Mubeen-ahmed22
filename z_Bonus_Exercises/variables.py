a259e306130c7db4844dd74546f2a56caf35b119
Mubeen-ahmed22 <mrmaheen123@gmail.com>
2024-11-22 14:41:49 +0400

    Create variables.py

---
+++ z_Bonus_Exercises/variables.py
@@ -0,0 +1,7 @@
+#Assign a message to a variable, and print that message. Then change the value of the variable to a new message, and print the new message.
+
+message = "We are venom"
+print(message)
+
+message = "I am ironman"
+print(message)
